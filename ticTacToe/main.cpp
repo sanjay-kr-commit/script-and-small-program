3635d9abbde6bbc4532d63c267a3cff6d8e3cd11
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-07-13 12:45:38 +0530

    add checker

--- ticTacToe/main.cpp
+++ ticTacToe/main.cpp
@@ -30,6 +30,54 @@ void printBoard( int *array ) {
     cout << "\n" ;
 }
 
+bool isGameOver( int *array , int *winner ) {
+
+    // horizontal check
+    for ( int i = 3 ; i < 10 ; i += 3 ) {
+        int s = i - 3 ;
+        bool emptySlot = false ;
+        for ( int j = s ; j < i && !emptySlot ; j++ ) emptySlot = array[j] == 0 ;
+        if ( emptySlot ) continue ;
+        bool match = true ;
+        s++ ;
+        for ( int j = s ; j < i && match ; j++ ) match = ( array[j] == array[j-1] ) ;
+        if ( match ) {
+            *winner = array[s] ;
+            return true ;
+        }
+    }
+
+    // vertical check
+    for ( int i = 0 ; i < 3 ; i++ ) {
+        bool emptySlot = false ;
+        for ( int j = i ; j < 9 && !emptySlot ; j += 3 ) emptySlot = array[j] == 0 ;
+        if ( emptySlot ) continue ;
+        bool match = true ;
+        for ( int j = i+3 ; j < 9 ; j += 3 ) match = array[j] == array[j-3] ;
+        if ( match ) {
+            *winner = array[i] ;
+            return true ;
+        }
+    }
+
+    // cross check
+    if ( array[4] != 0 && ((array[0] == array[4] && array[4] == array[8]) || ( array[2] == array[4] && array[4] == array[6] ))) {
+        *winner = array[4] ;
+        return true ;
+    }
+
+    // check for tie
+    bool slotFilled = true ;
+    for ( int i = 0 ; i < 9 && slotFilled ; i++ ) slotFilled = (array[i] != 0) ;
+
+    // check for tie
+    if ( slotFilled ) *winner = -1 ;
+
+    // result
+    return slotFilled ;
+
+}
+
 int main(int argc, char **argv) {
 
     int array[9] ;
@@ -54,7 +102,9 @@ int main(int argc, char **argv) {
         int t = 0 ;
         char invalid[] = "Invalid Index Try Again\n" ;
         char occupied[] = "Place is already occupied\n" ;
-        while ( true ) {
+        int winner ;
+
+        while ( !isGameOver( array , &winner ) ) {
             printBoard( array ) ;
 
             if ( t == 1 ) cout << invalid ;
@@ -76,6 +126,15 @@ int main(int argc, char **argv) {
             if ( t == 0 ) c = ( c == 1 ) ? 2 : 1 ;
 
         }
+
+        system("clear");
+        cout << ( ( winner == 0 ) ? "It's a tie" : ( ( winner == 1 ) ? "x won" : "o won" )  ) ;
+
+        op = 'n' ;
+        cout << "\nEnter 'y' to restart game : " ;
+        cin >> op ;
+        if ( op != 'y' ) return 0 ;
+
 
     }
 
